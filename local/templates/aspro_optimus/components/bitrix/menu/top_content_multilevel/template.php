ddd07f020ec010349500f522f4be060d7aa213d5
TimVance <2DngxvshA6GUGcQ>
2020-03-31 13:34:44 +0300

    Поиск над меню

--- local/templates/aspro_optimus/components/bitrix/menu/top_content_multilevel/template.php
+++ local/templates/aspro_optimus/components/bitrix/menu/top_content_multilevel/template.php
@@ -48,37 +48,6 @@
 					<?}?>
 				</li>
 			<?}?>
-            <li>
-                <div class="mobile-search-wrapper">
-                    <?$APPLICATION->IncludeComponent(
-                        "datainlife:iblocksearch",
-                        ".default",
-                        array(
-                            "CATALOG_FOLDER" => "/catalog/",
-                            "COMPOSITE_FRAME_MODE" => "A",
-                            "COMPOSITE_FRAME_TYPE" => "AUTO",
-                            "CONTAINER_ID" => "di-search",
-                            "EXT_FILTER" => "shopFilter",
-                            "INPUT_ID" => "di-search-input",
-                            "INPUT_SECTION_ID" => "di-search-section-id-input",
-                            "PRICE_CODE" => array(
-                                0 => "Розничная цена",
-                            ),
-                            "PROPERTY_CODE" => array(
-                            ),
-                            "RANDOM_ELEMENTS_COUNT" => "5",
-                            "RANDOM_ELEMENTS_PICTURE_FIELD" => "PREVIEW_PICTURE",
-                            "RANDOM_ELEMENTS_PICTURE_HEIGHT" => "100",
-                            "RANDOM_ELEMENTS_PICTURE_WIDTH" => "100",
-                            "RESULT_CONTAINER_ID" => "di-search-result",
-                            "SECTIONS_COUNT_TOP" => "6",
-                            "SECTION_ID" => "",
-                            "COMPONENT_TEMPLATE" => ".default"
-                        ),
-                        false
-                    );?>
-                </div>
-            </li>
 		</ul>
 	</div>
 <?}?>
